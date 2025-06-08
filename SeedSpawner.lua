e6a0030646a56c207a8b2ba0d003d998a70767b8
DarkNatch <jeckonifuji@gmail.com>
2025-06-08 20:35:53 +0800

    SeedSpawner.lua

---
+++ SeedSpawner.lua
@@ -0,0 +1,10 @@
+-- Seed Spawner Logic
+print("[SeedSpawner] Loaded")
+
+local function spawnSeed(seedType)
+    print("Spawning seed:", seedType)
+    -- TODO: Trigger actual garden planting logic
+end
+
+-- Example
+spawnSeed("Tomato")
