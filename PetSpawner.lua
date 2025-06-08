817cbd8fa5f2b188aa7668ac71292b8691739a2f
DarkNatch <jeckonifuji@gmail.com>
2025-06-08 20:35:11 +0800

    PetSpawner.lua

---
+++ PetSpawner.lua
@@ -0,0 +1,11 @@
+-- Pet Spawner Logic
+print("[PetSpawner] Loaded")
+
+-- Replace this with your actual pet spawning logic
+local function spawnPet(petName)
+    print("Spawning pet:", petName)
+    -- TODO: Insert the real server event to spawn a pet
+end
+
+-- Example
+spawnPet("Raccoon")
