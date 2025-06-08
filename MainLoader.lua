037800f4a4072c05717bded12a03242a30effe1d
DarkNatch <jeckonifuji@gmail.com>
2025-06-08 20:44:39 +0800

    MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -2,7 +2,7 @@ local Features = {
     EggHatchingDetect = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/refs/heads/main/EggHatchingDetect.lua",
     PetSpawner = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/refs/heads/main/PetSpawner.lua",
     SeedSpawner = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/refs/heads/main/SeedSpawner.lua",
-    ServerManager = "https://raw.githubusercontent.com/YourUsername/Grow-a-Garden-Scripts/main/ServerManager.lua",
+    ServerManager = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/refs/heads/main/ServerManager.lua",
     LoadingScreen = "https://raw.githubusercontent.com/YourUsername/Grow-a-Garden-Scripts/main/LoadingScreen.lua"
 }
 
